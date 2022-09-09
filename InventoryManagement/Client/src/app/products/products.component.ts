30e4ff6b5b621284927b85a976da2d25ec0b110a
Marcell Tanure <59565705+marcellpaganini@users.noreply.github.com>
2022-09-09 11:27:07 -0300

    client side pagination implemented

--- InventoryManagement/Client/src/app/products/products.component.ts
+++ InventoryManagement/Client/src/app/products/products.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from './../../environments/environment';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
 import { Product } from './product';
 
 
@@ -11,15 +13,17 @@ import { Product } from './product';
 })
 
 export class ProductsComponent implements OnInit {
-  public displayedColumns: string[] = ['name', 'description', 'price', 'quantity', 'status']
-  public products!: Product[];
+  public displayedColumns: string[] = ['name', 'description', 'price', 'status', 'quantity'];
+  public products!: MatTableDataSource<Product>;
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
 
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
     this.http.get<Product[]>(environment.baseUrl + 'api/Products')
       .subscribe(result => {
-        this.products = result;
+        this.products = new MatTableDataSource<Product>(result);
+        this.products.paginator = this.paginator;
       }, error => console.error(error));
   }
 }
