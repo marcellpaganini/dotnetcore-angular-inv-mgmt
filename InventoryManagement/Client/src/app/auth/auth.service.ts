a206e9db1787fba09f85ad2060a12c1424ff0f26
Marcell Tanure <59565705+marcellpaganini@users.noreply.github.com>
2022-09-27 17:12:09 -0300

    authStatus created

--- InventoryManagement/Client/src/app/auth/auth.service.ts
+++ InventoryManagement/Client/src/app/auth/auth.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, Subject, tap } from 'rxjs';
 import { environment } from './../../environments/environment';
 import { LoginRequest } from './login-request';
 import { LoginResult } from './login-result';
@@ -11,9 +11,12 @@ import { LoginResult } from './login-result';
 })
 
 export class AuthService {
-  constructor(protected http: HttpClient) { }
+  private tokenKey: string = "token";
 
-  public tokenKey: string = "token";
+  private _authStatus = new Subject<boolean>();
+  public authStatus = this._authStatus.asObservable();
+
+  constructor(protected http: HttpClient) { }
 
   isAuthenticated(): boolean {
     return this.getToken() !== null;
@@ -23,8 +26,29 @@ export class AuthService {
     return localStorage.getItem(this.tokenKey);
   }
 
+  init(): void {
+    if (this.isAuthenticated()) {
+      this.setAuthStatus(true);
+    }
+  }
+
   login(item: LoginRequest): Observable<LoginResult> {
     var url = environment.baseUrl + "api/Account/Login";
-    return this.http.post<LoginResult>(url, item);
+    return this.http.post<LoginResult>(url, item)
+      .pipe(tap(loginResult => {
+        if (loginResult.success && loginResult.token) {
+          localStorage.setItem(this.tokenKey, loginResult.token);
+          this.setAuthStatus(true);
+        }
+      }));
+  }
+
+  logout() {
+    localStorage.removeItem(this.tokenKey);
+    this.setAuthStatus(false);
+  }
+
+  private setAuthStatus(isAuthenticated: boolean): void {
+    this._authStatus.next(isAuthenticated);
   }
 }
