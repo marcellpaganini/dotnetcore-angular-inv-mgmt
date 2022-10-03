b8f050caeaadd64a21517da52d6991efcc4a6c75
Marcell Tanure <59565705+marcellpaganini@users.noreply.github.com>
2022-10-03 10:09:30 -0300

    Fetch-Data and weather forecast removed

--- InventoryManagement/Client/src/app/app.module.ts
+++ InventoryManagement/Client/src/app/app.module.ts
@@ -4,7 +4,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { HomeComponent } from './home/home.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -20,7 +19,6 @@ import { LoginComponent } from './auth/login.component';
   declarations: [
     AppComponent,
     HomeComponent,
-    FetchDataComponent,
     NavMenuComponent,
     ProductsComponent,
     SuppliersComponent,
